f3e7381459b0087a84a9fbd763c6cc727e2658dd
Sophie Wirth <a.sophiewirth@gmail.com>
2019-02-07 16:33:15 +0000

    Start on header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import configureStore from '../../../configureStore';
 import '../../style/style.scss';
 import AppRouter from './AppRouter';
+import Header from '../Header';
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <Provider store={configureStore()}>
         <div className="main">
+          <Header />
           <AppRouter />
         </div>
       </Provider>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import { ReduxState } from '../../redux/reducers';
+import { ViewState, Views } from '../../redux/reducers/viewReducers';
+
+import menuSVG from '../../images/barsSolid.svg';
+
+interface StateProps {
+  views: ViewState;
+}
+
+interface DispatchProps {
+}
+
+type Props = StateProps & DispatchProps;
+
+class Header extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div className="header">
+        <div className="header__logo">PunchClock</div>
+        <div className="header__navigation">
+          <span className={`header__navigation-item ${this.props.views.activeView === Views.Logger ? 'active' : ''}`}>Logger</span>
+          <span className={`header__navigation-item disabled`}>Dashboard</span>
+        </div>
+        <div className="header__menu">
+          <div
+            className="header__menu-icon"
+            onClick={() => { }}>
+            <img src={menuSVG} alt="menu-icon" />
+          </div>
+          <ul className="header__menu-items">
+            <li className="menu-item">Logout</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state: ReduxState): StateProps {
+  return {
+    views: state.views,
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+  return {
+  };
+}
+
+export default connect<StateProps, DispatchProps, {}, ReduxState>(mapStateToProps, mapDispatchToProps)(Header);
